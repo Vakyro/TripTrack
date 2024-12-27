2ca14a5247b4ee342f80b1ae14d9faf10d196efb
Leonardo <cascalderonleonardo@gmail.com>
2024-12-27 13:29:16 -0800

    Update layout.tsx le quite las view-transitions

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { Toaster } from 'sonner' // Importa el componente Toaster
-import { Link } from 'next-view-transitions'
+import Link from 'next/link'
 import { useRouter, usePathname } from 'next/navigation'
 import { Button } from "@/components/ui/button"
 import { Home, Truck, PenToolIcon as Tool, AlertTriangle, User, MessageCircle } from 'lucide-react'
